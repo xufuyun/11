142d1abff38fade64c0f508c75dd12bbe5030d04
596953436@qq.com <xufyun>
2018-11-29 11:32:01 +0800

    2018-11-9-2

--- 3.py
+++ 3.py
@@ -1,1 +1,19 @@
-#fff
+#!/usr/bin/python3
+#-*- coding:UTF-8 -*-
+class Student():
+    pass
+#定义一个空类，此处pass必须有
+minyue=Student()
+#再定义一个类，用来描叙Python的学生
+class PythonStudent():
+    name = None
+    age = 18
+    course = "python"
+    def doHomework(self):
+        print("正在做笔记")
+        return 
+#实例化
+mingyue = PythonStudent()
+print(mingyue.name)
+print(mingyue.age)
+mingyue.doHomework()
